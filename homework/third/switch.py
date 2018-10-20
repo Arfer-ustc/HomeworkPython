d08bf402aae808af58d82c30d94f3924b4fd651f
xuefei <1403193782@qq.com>
2018-10-20 21:25:33 +0800

    home3

---
+++ homework/third/switch.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def showGray(inputfile,gray):
+
+
+    plt.figure('color')
+
+    plt.imshow(gray, cmap='gray')
+
+    plt.axis('off')
+
+    plt.show()
+
+
+
+def savaGray(outputfile,gray):
+    gray.save(outputfile)
+
+
+def main():
+    #需要进行灰度转换的原彩色图片存储位置
+    inputfile='color.gif'
+
+    #需要保存的转换后的图片名
+    outputfile='gray.gif'
+
+    img = Image.open(inputfile)
+
+    gray = img.convert('L')
+
+    #显示灰色图片
+    showGray(inputfile,gray)
+
+    #保存灰色图片
+    savaGray(outputfile,gray)
+
+
+if __name__ == '__main__':
+    main()
